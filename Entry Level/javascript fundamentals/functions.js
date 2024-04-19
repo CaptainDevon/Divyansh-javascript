87abc58bf3d2384ccb0433925097b374e5d3e1a5
CaptainDevon <divyansh2004ind@gmail.com>
2024-04-19 22:05:37 +0530

    functions

---
+++ Entry Level/javascript fundamentals/functions.js
@@ -0,0 +1,17 @@
+//function declaration
+function isinputvalid(input)
+{
+    console.log("validating department");
+}
+
+isinputvalid("marketing");
+
+
+function addition(a,b)
+{
+    return a+b;
+}
+let result1=addition(2,3);
+let result2=addition(2,"3");
+console.log(result1);
+console.log(result2);
