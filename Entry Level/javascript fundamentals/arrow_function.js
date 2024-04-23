54e837591c44bae853e98629007747daaf533349
CaptainDevon <divyansh2004ind@gmail.com>
2024-04-23 12:39:34 +0530

    arrow function

--- Entry Level/javascript fundamentals/arrow_function.js
+++ Entry Level/javascript fundamentals/arrow_function.js
@@ -1,6 +1,29 @@
-let greet=function()
+let greet=(user)=>
 {
-    console.log(`hello world`);
-    return 1;
+    console.log(`hello ${user} how are you?`);
 }
-console.log(greet());
+
+let name='divyansh sinha';
+let result=greet(name);
+
+
+let sort=(array)=>{
+    for(let i=0;i<array.length;i++)
+    {
+        for(let j=i+1;j<array.length;j++)
+        {
+            if(array[j]<array[i])
+            {
+                let temp=array[i];
+                array[i]=array[j];
+                array[j]=temp;
+            }
+        }
+    }
+    return array;
+}
+
+let arr=[-9,6,0,-1,5,-3,-10,11];
+console.log(arr);
+let sorted=sort(arr);
+console.log(sorted);
