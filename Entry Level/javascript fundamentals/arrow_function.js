0a385f8eaeecd85bdca3f55e08b1fed2873c4bd3
CaptainDevon <divyansh2004ind@gmail.com>
2024-04-23 12:23:13 +0530

    functions

---
+++ Entry Level/javascript fundamentals/arrow_function.js
@@ -0,0 +1,6 @@
+let greet=function()
+{
+    console.log(`hello world`);
+    return 1;
+}
+console.log(greet());
--- Entry Level/javascript fundamentals/functions.js
+++ Entry Level/javascript fundamentals/functions.js
@@ -1,17 +1,53 @@
-//function declaration
-function isinputvalid(input)
+//making of average of he elements in an array 
+function average_value(arr)
 {
-    console.log("validating department");
+    let sum=0;
+    for(i in arr)
+    {
+        sum+=arr[i];
+    }
+    return sum/(arr.length);
+}
+let arr=[50,46,46,30,60];
+let result=average_value(arr);
+console.log(`average score: ${result}`);
+
+function plus_avg(a,b)
+{
+    console.log('done');
+    return (1+a+b)/2;
 }
 
-isinputvalid("marketing");
+let a=1;
+let b=2;
+let c=3;
+
+let result1=plus_avg(a,b);
+let result2=plus_avg(b,c);
+let result3=plus_avg(c,a);
+let array=[result1,result2,result3];
+console.log(array);
 
 
-function addition(a,b)
+//making of a dsa function
+function selection_sort(array)
 {
-    return a+b;
+    for(let i=0;i<array.length;i++)
+    {
+        for(let j=i+1;j<array.length;j++)
+        {
+            if(array[i]>array[j])
+            {
+                let temp=array[i];
+                array[i]=array[j];
+                array[j]=temp;
+            }
+        }
+    }
+    return array
 }
-let result1=addition(2,3);
-let result2=addition(2,"3");
-console.log(result1);
-console.log(result2);
+
+let array1=[1,2,5,3,6,7,-3,-8,0,9];
+
+let resultant=selection_sort(array1);
+console.log(`resultant: ${resultant}`);
